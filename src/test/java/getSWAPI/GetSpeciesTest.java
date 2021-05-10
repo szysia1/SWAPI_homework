2a0722574a27a9cea24f90e1dd54f0d81584a156
mszyszkowska <m.szyszkowska@madkom.pl>
2021-05-11 00:07:09 +0200

    get spieces test

--- src/test/java/getSWAPI/GetSpeciesTest.java
+++ src/test/java/getSWAPI/GetSpeciesTest.java
@@ -1,13 +1,11 @@
 package getSWAPI;
 
 import base.BaseTest;
-import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,10 +35,29 @@ public class GetSpeciesTest extends BaseTest {
                 .response();
 
         JsonPath json = response.jsonPath();
-        List<String> films = json.getList("films");
 
         assertThat(json.getString("name")).isEqualTo("Wookiee");
-        assertThat(films).hasSize(5).contains("https://swapi.py4e.com/api/films/1/","https://swapi.py4e.com/api/films/2/","https://swapi.py4e.com/api/films/3/","https://swapi.py4e.com/api/films/6/","https://swapi.py4e.com/api/films/7/");
+        assertThat(json.getString("classification")).isEqualTo("mammal");
+        assertThat(json.getString("designation")).isEqualTo("sentient");
+        assertThat(json.getString("average_height")).isEqualTo("210");
+        assertThat(json.getString("skin_colors")).isEqualTo("gray");
+        assertThat(json.getString("hair_colors")).isEqualTo("black, brown");
+        assertThat(json.getString("eye_colors")).isEqualTo("blue, green, yellow, brown, golden, red");
+        assertThat(json.getString("average_lifespan")).isEqualTo("400");
+        assertThat(json.getString("homeworld")).isEqualTo("https://swapi.py4e.com/api/planets/14/");
+        assertThat(json.getString("language")).isEqualTo("Shyriiwook");
+        assertThat(json.getList("people")).hasSize(2).containsExactly(
+                "https://swapi.py4e.com/api/people/13/",
+                "https://swapi.py4e.com/api/people/80/");
+        assertThat(json.getList("films")).hasSize(5).contains(
+                "https://swapi.py4e.com/api/films/1/",
+                "https://swapi.py4e.com/api/films/2/",
+                "https://swapi.py4e.com/api/films/3/",
+                "https://swapi.py4e.com/api/films/6/",
+                "https://swapi.py4e.com/api/films/7/");
+        assertThat(json.getString("created")).isEqualTo("2014-12-10T16:44:31.486000Z");
+        assertThat(json.getString("edited")).isEqualTo("2014-12-20T21:36:42.142000Z");
+        assertThat(json.getString("url")).isEqualTo("https://swapi.py4e.com/api/species/3/");
 
     }
 }
