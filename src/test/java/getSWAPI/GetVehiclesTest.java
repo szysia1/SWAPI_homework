38a9456e1909f7a524b3e8185228cd8edb4a6d22
mszyszkowska <m.szyszkowska@madkom.pl>
2021-05-11 00:20:54 +0200

    get vehicles test

--- src/test/java/getSWAPI/GetVehiclesTest.java
+++ src/test/java/getSWAPI/GetVehiclesTest.java
@@ -1,15 +1,11 @@
 package getSWAPI;
 
 import base.BaseTest;
-import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
-import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.*;
@@ -39,10 +35,24 @@ public class GetVehiclesTest extends BaseTest {
                 .response();
 
         JsonPath json = response.jsonPath();
-        List<String> films = json.getList("films");
 
         assertThat(json.getString("name")).isEqualTo("Sand Crawler");
-        assertThat(films).hasSize(2).contains("https://swapi.py4e.com/api/films/1/", "https://swapi.py4e.com/api/films/5/");
-
+        assertThat(json.getString("model")).isEqualTo("Digger Crawler");
+        assertThat(json.getString("manufacturer")).isEqualTo("Corellia Mining Corporation");
+        assertThat(json.getString("cost_in_credits")).isEqualTo("150000");
+        assertThat(json.getString("length")).isEqualTo("36.8 ");
+        assertThat(json.getString("max_atmosphering_speed")).isEqualTo("30");
+        assertThat(json.getString("crew")).isEqualTo("46");
+        assertThat(json.getString("passengers")).isEqualTo("30");
+        assertThat(json.getString("cargo_capacity")).isEqualTo("50000");
+        assertThat(json.getString("consumables")).isEqualTo("2 months");
+        assertThat(json.getString("vehicle_class")).isEqualTo("wheeled");
+        assertThat(json.getList("pilots")).isEmpty();
+        assertThat(json.getList("films")).hasSize(2).contains(
+                "https://swapi.py4e.com/api/films/1/",
+                "https://swapi.py4e.com/api/films/5/");
+        assertThat(json.getString("created")).isEqualTo("2014-12-10T15:36:25.724000Z");
+        assertThat(json.getString("edited")).isEqualTo("2014-12-20T21:30:21.661000Z");
+        assertThat(json.getString("url")).isEqualTo("https://swapi.py4e.com/api/vehicles/4/");
     }
 }
