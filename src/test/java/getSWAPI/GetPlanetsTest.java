30f7237ac7e40799ea2774a99f3794ef6d0cc5e7
mszyszkowska <m.szyszkowska@madkom.pl>
2021-05-10 23:57:22 +0200

    get planet test

--- src/test/java/getSWAPI/GetPlanetsTest.java
+++ src/test/java/getSWAPI/GetPlanetsTest.java
@@ -1,13 +1,11 @@
 package getSWAPI;
 
 import base.BaseTest;
-import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,11 +35,36 @@ public class GetPlanetsTest extends BaseTest {
                 .response();
 
         JsonPath json = response.jsonPath();
-        List<String> films = json.getList("films");
 
         assertThat(json.getString("name")).isEqualTo("Tatooine");
-        assertThat(films).hasSize(5).contains("https://swapi.py4e.com/api/films/1/", "https://swapi.py4e.com/api/films/3/", "https://swapi.py4e.com/api/films/4/", "https://swapi.py4e.com/api/films/5/", "https://swapi.py4e.com/api/films/6/"
+        assertThat(json.getString("rotation_period")).isEqualTo("23");
+        assertThat(json.getString("orbital_period")).isEqualTo("304");
+        assertThat(json.getString("diameter")).isEqualTo("10465");
+        assertThat(json.getString("climate")).isEqualTo("arid");
+        assertThat(json.getString("gravity")).isEqualTo("1 standard");
+        assertThat(json.getString("terrain")).isEqualTo("desert");
+        assertThat(json.getString("surface_water")).isEqualTo("1");
+        assertThat(json.getString("population")).isEqualTo("200000");
+        assertThat(json.getList("residents")).hasSize(10).contains(
+                "https://swapi.py4e.com/api/people/1/",
+                "https://swapi.py4e.com/api/people/2/",
+                "https://swapi.py4e.com/api/people/4/",
+                "https://swapi.py4e.com/api/people/6/",
+                "https://swapi.py4e.com/api/people/7/",
+                "https://swapi.py4e.com/api/people/8/",
+                "https://swapi.py4e.com/api/people/9/",
+                "https://swapi.py4e.com/api/people/11/",
+                "https://swapi.py4e.com/api/people/43/",
+                "https://swapi.py4e.com/api/people/62/");
+        assertThat(json.getList("films")).hasSize(5).contains(
+                "https://swapi.py4e.com/api/films/1/",
+                "https://swapi.py4e.com/api/films/3/",
+                "https://swapi.py4e.com/api/films/4/",
+                "https://swapi.py4e.com/api/films/5/",
+                "https://swapi.py4e.com/api/films/6/"
         );
-
+        assertThat(json.getString("created")).isEqualTo("2014-12-09T13:50:49.641000Z");
+        assertThat(json.getString("edited")).isEqualTo("2014-12-20T20:58:18.411000Z");
+        assertThat(json.getString("url")).isEqualTo("https://swapi.py4e.com/api/planets/1/");
     }
 }
